6da012ed2bd4e6ee2bf3eea162f564a8bcbcfaaa
b216014 <b216014@iiit-bh.ac.in>
2020-03-09 15:58:39 +0530

    Added a test ase to the cart container.

--- src/Cart/index.test.js
+++ src/Cart/index.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import axios from 'axios';
 import Cart from './index';
 
 describe('the cart page container', () => {
@@ -9,5 +10,12 @@ describe('the cart page container', () => {
   it('should render correctly', () => {
     const { asFragment } = render(<Router history={history}><Cart /></Router>);
     expect(asFragment()).toMatchSnapshot();
+  });
+  it('should make an api call when rendered', () => {
+    const mockAxios = jest.spyOn(axios, 'get');
+    mockAxios.mockResolvedValue({ data: [] });
+
+    render(<Router history={history}><Cart /></Router>);
+    expect(mockAxios).toHaveBeenCalledWith('http://localhost:8080/products');
   });
 });
