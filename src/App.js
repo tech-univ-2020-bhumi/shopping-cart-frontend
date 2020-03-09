f07e1efd696a7954522958a158dafeefde42ae86
b216014 <b216014@iiit-bh.ac.in>
2020-03-09 15:33:25 +0530

    fix(App): Fixed the app component to include routes and render correctly.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
-import Header from './components/Header';
 import ProductsPage from './ProductsPage';
 
-const App = () => (
-  <div className="App">
-    <Header testId="header-test" />
-    <ProductsPage />
-  </div>
+
+library.add(
+  fab,
+  faShoppingCart,
 );
+const App = () => {
+  const [cartCount, setCartCount] = useState(0);
+  return (
+    <div className="App">
+      <Router>
+        <div className="header">
+          <Link to="/cart">
+            <button type="button" className="cart-icon">
+              <FontAwesomeIcon icon="shopping-cart" />
+            </button>
+          </Link>
+        </div>
+        <div className="app-body">
+          <Switch>
+            <Route exact path="/cart" />
+            <Route path="/">
+              <ProductsPage
+                cartCount={cartCount}
+                setCartCount={setCartCount}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
