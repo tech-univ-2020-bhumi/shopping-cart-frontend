308b63acc62f52a518c621a3d8b6e049f35f1b7c
b216014 <b216014@iiit-bh.ac.in>
2020-03-09 23:57:41 +0530

    Modified filter components tests.

--- src/hooks/filteredProducts.test.js
+++ src/hooks/filteredProducts.test.js
@@ -11,4 +11,36 @@ describe('the useFilteredProducts hook', () => {
     expect(mockAxios).toHaveBeenCalledWith('http://localhost:8080/products');
     expect(result.current[0]).toEqual({});
   });
+
+  it('should calculate the total cost from the products', async () => {
+    const mockAxios = jest.spyOn(axios, 'get');
+    mockAxios.mockResolvedValue({ data: [] });
+    const {
+      result,
+      waitForNextUpdate,
+    } = renderHook(() => useFilteredProducts());
+    await waitForNextUpdate();
+    expect(result.current[1]).toEqual(0);
+  });
+
+  it('should calculate the total cart count from the products', async () => {
+    const mockAxios = jest.spyOn(axios, 'get');
+    mockAxios.mockResolvedValue({ data: [] });
+    const {
+      result,
+      waitForNextUpdate,
+    } = renderHook(() => useFilteredProducts());
+    await waitForNextUpdate();
+    expect(result.current[2]).toEqual(0);
+  });
+  it('should return the product list from the response', async () => {
+    const mockAxios = jest.spyOn(axios, 'get');
+    mockAxios.mockResolvedValue({ data: [] });
+    const {
+      result,
+      waitForNextUpdate,
+    } = renderHook(() => useFilteredProducts());
+    await waitForNextUpdate();
+    expect(result.current[3]).toEqual([]);
+  });
 });
